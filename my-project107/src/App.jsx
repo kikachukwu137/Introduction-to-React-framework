55dac3d7a3d62bd29490eb77812596a93aa59cd7
Egwaoje Daniel <egwaoje.daniel@gmail.com>
2025-07-14 12:20:12 +0100

    form react

--- my-project107/src/App.jsx
+++ my-project107/src/App.jsx
@@ -1,29 +1,77 @@
+// import { useState } from "react";
+
+// function App(){
+//   const [isMouseOver, setMouseOver] = useState(false)
+//   function handleMouseOver(){
+//     setMouseOver(true)
+//   }
+//   function handleMouseOut(){
+//     setMouseOver(false)
+//   }
+//   return(
+//     <div className="container">
+//       <input type="text" placeholder="what is your name
+//       " />
+//       <button 
+//         onMouseOver={handleMouseOver}
+//         onMouseOut={handleMouseOut}
+//         style={{backgroundColor: isMouseOver ? "black":"white" }}
+//         >submit</button>
+//     </div>
+
+//   )
+// }
+
+// export default App
+
+
+
+
 import { useState } from "react";
 
 function App(){
-  const [heading, setHeading] = useState("hello")
-  const[isMoveOver ,setIsMoveOver] = useState(false)
+  const [name,setName] = useState("");
+  const [heading, setHeading] = useState("");
 
-  function Heading(){
-    setHeading("submitted");
+  function handleClick(event){
+    setName(event.target.value)
+  }
+  function handleChange(event){
+    setHeading(name)
+    event.preventDefault()
+
   }
 
-  function handleClick(){
-    
-    setIsMoveOver(true)
-  }
-  function returnState(){
-    setIsMoveOver(false)
-  }
-  
-  return(
+  return (
     <div className="container">
-      <h1>{heading}</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button  onClick={Heading} onMouseOver={handleClick} onMouseOut={returnState}  style={{backgroundColor:isMoveOver ? "black" : "white"}}>submit</button>
-    </div>
+      <h1>hello {heading}</h1>
+      <form onSubmit={handleChange}>
+      <input onChange={handleClick}
+        type="text"
+         placeholder="what is your name"
+         value={name}
+        
+        />
+        <button>submit</button>
+        </form>
 
+    </div>
   )
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
